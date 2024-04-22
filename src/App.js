9fd8193f25fdff281b7fe6110095377d0b9f0fd1
MYSHA-R <164071821+MYSHA-R@users.noreply.github.com>
2024-04-22 08:41:49 -0500

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,26 +1,58 @@
-import React from 'react';
 import './App.css';
-import Home from './components/Home/Home';
-import Sidebar from './components/Sidebar/Sidebar';
-import Note from './components/Note/Note';
-import LabAction from './components/LabAction/LabAction';
-import Message from './components/Message/Message';
-import Lab from './components/Lab/Lab';
+import './index.css';
+import "./HisApp.css";
+import "./ToDoApp.css"
+import "./Kidney_Func.css"
+import "./PathologicalTest.css"
+import "./LiverEny.css"
+import History from './History'
+import ToDo from './ToDoApp'
+import BloodTest from './BloodTest'
+import  Kidney_Func from './Kidney_Func'
+import PathologicalTest from './PathologicalTest'
+import LiverEny from './LiverEny'
+import XRay from './XRay'
+import CompleteBloodTest from './CompleteBloodTest'
+import CompletePathoTest from './CompletePathoTest'
+
+import {BrowserRouter, Routes, Route, useNavigate  } from 'react-router-dom';
+import Lab from './Lab';
 
 
-const App = () => {
+
+function Router(){
   return(
-    <>
-    < LabAction />
-    <main className = "main">
-      < Lab />
-      < Sidebar />
-      < Home />
-      < Note />
-      < Message />
-    </main>
-    </>
-    );
+    <BrowserRouter>
+          <Routes> 
+            <Route path="/" element={<Lab/>}></Route>
+            <Route path="/hist" element={<History/>}></Route>
+            <Route path="/todo" element={<ToDo/>}></Route>
+            <Route path="/bloodtest" element={<BloodTest/>}></Route>
+            <Route path="/Kidney_Func" element={<Kidney_Func/>}></Route>
+            <Route path="/pathologitest" element={<PathologicalTest/>}></Route>
+            <Route path="/livereny" element={<LiverEny/>}></Route>
+            <Route path="/xray" element={<XRay/>}></Route>
+            <Route path="/completebloodtest" element={<CompleteBloodTest/>}></Route>
+            <Route path="/completepathotest" element={<CompletePathoTest/>}></Route>
+          </Routes>
+      </BrowserRouter>
+  );
+}
+
+      <div className='nav__menu'>
+       <ul className='nav__list'>
+       <li className='nav__item'>
+          </li>
+
+       </ul>
+      </div>
+  
+
+
+const App=()=> {
+  return (
+    <Router></Router>
+  );
 }
 
 export default App;
